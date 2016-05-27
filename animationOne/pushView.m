5b89f120f9bfe7ccc99681ec8878d7bb92fc3900
zhanming <18611205315@163.com>
2016-05-27 18:12:05 +0800

    添加了按钮点击

--- animationOne/pushView.m
+++ animationOne/pushView.m
@@ -10,7 +10,13 @@
 #import "pushButton.h"
 
 #define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
-
+typedef NS_ENUM(NSInteger, ZMButtonType) {
+    ZMButtonTypeCamera = 0,
+    ZMButtonTypePicture,
+    ZMButtonTypeResell
+    
+    
+};
 @interface pushView()
 
 @property(assign,nonatomic)BOOL isAnimation;
@@ -18,12 +24,23 @@
 @property(strong,nonatomic)UIWindow *keyWindow;
 @property(strong,nonatomic)NSMutableArray<pushButton*> *btnArray;
 @property(strong,nonatomic)NSMutableArray *btnRectArray;
+@property(strong,nonatomic)NSMutableArray *fromRectArray;
 @property(strong,nonatomic)UIButton *addBtn;
 
 @end
 
 @implementation pushView
 
+
+-(NSMutableArray *)fromRectArray
+{
+    if(_fromRectArray == nil)
+    {
+        _fromRectArray=[NSMutableArray new];
+        
+    }
+    return _fromRectArray;
+}
 
 -(NSMutableArray<pushButton *> *)btnArray
 {
@@ -51,7 +68,7 @@
     if (self) {
         
         self.frame=CGRectMake(0, 0, ScreenWidth,ScreenHeight);
-  
+        
         self.isAnimation=NO;
         
         
@@ -94,19 +111,41 @@
         
         pushButton *btn=[pushButton buttonWithType:UIButtonTypeCustom];
         
+        [btn addTarget:self action:@selector(blick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        btn.tag=i;
+        
         //使按钮的frame和加号按钮的frame一样
         btn.frame=CGRectMake(0, 0, self.addBtn.frame.size.width, self.addBtn.frame.size.width);
         btn.center=self.addBtn.center;
         
         
         [btn setImage:[UIImage imageNamed:[NSString stringWithFormat:@"post_animate_%lu",(unsigned long)(i+1)]] forState:UIControlStateNormal];
-
+        
         //把按钮放到数组里，以便时候使用
         [self.btnArray addObject:btn];
         
         [self addSubview:btn];
         
         
+    }
+}
+
+-(void)blick:(UIButton *)btn
+{
+    switch (btn.tag) {
+        case ZMButtonTypeCamera:
+            NSLog(@"相机");
+            break;
+            
+        case ZMButtonTypePicture:
+            NSLog(@"图片");
+            break;
+        case ZMButtonTypeResell:
+            NSLog(@"一键转卖");
+            break;
+        default:
+            break;
     }
 }
 
@@ -119,7 +158,7 @@
         self.isAnimation=YES;
         CGFloat margin =40;
         CGFloat width=(ScreenWidth-margin*4)/3.0;
-
+        
         //这个是让加号旋转45度，没有难度吧
         [UIView animateWithDuration:0.3 animations:^{
             
@@ -129,67 +168,72 @@
         
         for (NSInteger i = 0; i < self.btnArray.count; i++) {
             
-                //按钮要到的位置的frame
-                CGRect toValue =CGRectMake(margin*(i+1)+width*i+width/2, ScreenHeight*0.7+width/2, width, width);
-                
-                pushButton *menuButton = self.btnArray[i];
+            //按钮要到的位置的frame
+            CGRect toValue =CGRectMake(margin*(i+1)+width*i+width/2, ScreenHeight*0.7+width/2, width, width);
             
-                //从小到大
-                CABasicAnimation *animationScale=[CABasicAnimation animation];
-                animationScale.keyPath=@"transform.scale";
-                animationScale.toValue=@(1.3);
-                //透明度不断增大
-                CABasicAnimation *animationoPacity=[CABasicAnimation animation];
-                animationoPacity.keyPath=@"opacity";
-                animationoPacity.fromValue=@(0);
-                animationoPacity.toValue=@(1);
-                //按钮有旋转
-                CABasicAnimation *animationRotation=[CABasicAnimation animation];
-                animationRotation.keyPath=@"transform.rotation.z";
+            [self.fromRectArray addObject:[NSValue valueWithCGRect:toValue]];
             
-                    //根据按钮的出现顺序旋转的角度也不同
-                animationRotation.fromValue=@(DEGREES_TO_RADIANS(90/(self.btnArray.count-i)));
-                animationRotation.toValue=@(0);
-              //有弹性效果
+            pushButton *menuButton = self.btnArray[i];
             
-              /*
-              * mass:
-              质量，影响图层运动时的弹簧惯性，质量越大，弹簧拉伸和压缩的幅度越大
-              * stiffness:
-              刚度系数(劲度系数/弹性系数)，刚度系数越大，形变产生的力就越大，运动越快
+            
+            
+            //从小到大
+            CABasicAnimation *animationScale=[CABasicAnimation animation];
+            animationScale.keyPath=@"transform.scale";
+            animationScale.toValue=@(1.3);
+            //透明度不断增大
+            CABasicAnimation *animationoPacity=[CABasicAnimation animation];
+            animationoPacity.keyPath=@"opacity";
+            animationoPacity.fromValue=@(0);
+            animationoPacity.toValue=@(1);
+            //按钮有旋转
+            CABasicAnimation *animationRotation=[CABasicAnimation animation];
+            animationRotation.keyPath=@"transform.rotation.z";
+            
+            //根据按钮的出现顺序旋转的角度也不同
+            animationRotation.fromValue=@(DEGREES_TO_RADIANS(90/(self.btnArray.count-i)));
+            animationRotation.toValue=@(0);
+            //有弹性效果
+            
+            /*
+             * mass:
+             质量，影响图层运动时的弹簧惯性，质量越大，弹簧拉伸和压缩的幅度越大
+             * stiffness:
+             刚度系数(劲度系数/弹性系数)，刚度系数越大，形变产生的力就越大，运动越快
              
-              * damping:
-              阻尼系数，阻止弹簧伸缩的系数，阻尼系数越大，停止越快
+             * damping:
+             阻尼系数，阻止弹簧伸缩的系数，阻尼系数越大，停止越快
              
-              * initialVelocity:
-              初始速率，动画视图的初始速度大小
-              速率为正数时，速度方向与运动方向一致，速率为负数时，速度方向与运动方向相反
-              */
+             * initialVelocity:
+             初始速率，动画视图的初始速度大小
+             速率为正数时，速度方向与运动方向一致，速率为负数时，速度方向与运动方向相反
+             */
             
             
-                CASpringAnimation *animationSpring=[CASpringAnimation animationWithKeyPath:@"position"];
-                animationSpring.damping =8;
-                animationSpring.stiffness = 120;
-                animationSpring.mass = 0.6;
-                animationSpring.initialVelocity = 0;
-                animationSpring.toValue =[NSValue valueWithCGPoint:toValue.origin];
+            CASpringAnimation *animationSpring=[CASpringAnimation animationWithKeyPath:@"position"];
+            animationSpring.damping =8;
+            animationSpring.stiffness = 120;
+            animationSpring.mass = 0.6;
+            animationSpring.initialVelocity = 0;
+            animationSpring.toValue =[NSValue valueWithCGPoint:toValue.origin];
             
-        
-                CAAnimationGroup *animationGroup=[CAAnimationGroup animation];
-                
-                animationGroup.animations=@[animationoPacity,animationScale,animationRotation,animationSpring];
-                animationGroup.duration=0.5;
-                //让动画延迟执行
-                animationGroup.beginTime=CACurrentMediaTime()+i*(0.4/self.btnArray.count);
-                
-                animationGroup.removedOnCompletion=NO;
-               
-                animationGroup.fillMode=kCAFillModeForwards;
+            
+            CAAnimationGroup *animationGroup=[CAAnimationGroup animation];
+            
+            animationGroup.animations=@[animationoPacity,animationScale,animationRotation,animationSpring];
+            animationGroup.duration=0.5;
+            //让动画延迟执行
+            animationGroup.beginTime=CACurrentMediaTime()+i*(0.4/self.btnArray.count);
+            
+            animationGroup.removedOnCompletion=NO;
+            
+            animationGroup.fillMode=kCAFillModeForwards;
+            animationGroup.delegate=self;
+            
+            [menuButton.layer addAnimation:animationGroup forKey:[NSString stringWithFormat:@"animation%ld",(long)i]];
+            
 
-                [menuButton.layer addAnimation:animationGroup forKey:[NSString stringWithFormat:@"animation%ld",(long)i]];
-                
-
-
+            
         }
     }
     
@@ -202,63 +246,61 @@
 
 -(void)closePushView
 {
- 
+    
     if(self.isAnimation)
     {
-       self.isAnimation=NO;
-       
+        self.isAnimation=NO;
+        
         [UIView animateWithDuration:0.3 animations:^{
             
             self.addBtn.transform=CGAffineTransformIdentity;
         }];
-  
-      for (NSInteger i = self.btnArray.count-1; i >=0; i--) {
-    
-       pushButton *menuButton = self.btnArray[i];
-          
-       CABasicAnimation *animationoPacity=[CABasicAnimation animation];
-       animationoPacity.keyPath=@"opacity";
-       animationoPacity.toValue=@(1);
-       animationoPacity.toValue=@(0.5);
-       
-       CABasicAnimation *animationScale=[CABasicAnimation animation];
-       
-       animationScale.keyPath=@"transform.scale";
-       animationScale.toValue=@(0.7);
-       
-       
-       CABasicAnimation *animationRotation=[CABasicAnimation animation];
-       
-       animationRotation.keyPath=@"transform.rotation.z";
-       
-       //animationRotation.fromValue=@(0);
-       animationRotation.toValue=@(DEGREES_TO_RADIANS(90/(self.btnArray.count-i)));
-       
-       CABasicAnimation *animationPosition=[CABasicAnimation animationWithKeyPath:@"position"];
-       
-       animationPosition.toValue =[NSValue valueWithCGPoint:self.addBtn.center];
-       CAAnimationGroup *animationGroup=[CAAnimationGroup animation];
-       
-       animationGroup.animations=@[animationPosition,animationScale,animationoPacity];
-
-       animationGroup.duration=0.23;
-          
-       animationGroup.beginTime=CACurrentMediaTime()+(self.btnArray.count-1-i)*(0.4/self.btnArray.count);
-       
-       animationGroup.removedOnCompletion=NO;
-       
-       animationGroup.fillMode=kCAFillModeForwards;
-       if(i==0)
-       {
-          animationGroup.delegate=self;
-       }
-
-      
-       [menuButton.layer addAnimation:animationGroup forKey:[NSString stringWithFormat:@"animationa%ld",(long)i]];
-       
-
-          
-      }
+        
+        for (NSInteger i = self.btnArray.count-1; i >=0; i--) {
+            
+            pushButton *menuButton = self.btnArray[i];
+            
+            CABasicAnimation *animationoPacity=[CABasicAnimation animation];
+            animationoPacity.keyPath=@"opacity";
+            animationoPacity.toValue=@(1);
+            animationoPacity.toValue=@(0.5);
+            
+            CABasicAnimation *animationScale=[CABasicAnimation animation];
+            
+            animationScale.keyPath=@"transform.scale";
+            animationScale.toValue=@(0.7);
+            
+            
+            CABasicAnimation *animationRotation=[CABasicAnimation animation];
+            
+            animationRotation.keyPath=@"transform.rotation.z";
+            
+            //animationRotation.fromValue=@(0);
+            animationRotation.toValue=@(DEGREES_TO_RADIANS(90/(self.btnArray.count-i)));
+            
+            CABasicAnimation *animationPosition=[CABasicAnimation animationWithKeyPath:@"position"];
+            
+            animationPosition.toValue =[NSValue valueWithCGPoint:self.addBtn.center];
+            CAAnimationGroup *animationGroup=[CAAnimationGroup animation];
+            
+            animationGroup.animations=@[animationPosition,animationScale,animationoPacity];
+            
+            animationGroup.duration=0.23;
+            
+            animationGroup.beginTime=CACurrentMediaTime()+(self.btnArray.count-1-i)*(0.4/self.btnArray.count);
+            
+            animationGroup.removedOnCompletion=NO;
+            
+            animationGroup.fillMode=kCAFillModeForwards;
+            if(i==0)
+            {
+                animationGroup.delegate=self;
+            }
+            
+            
+            [menuButton.layer addAnimation:animationGroup forKey:[NSString stringWithFormat:@"animationaClose%ld",(long)i]];
+            
+        }
     }
     else
     {
@@ -272,8 +314,40 @@
 
 -(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
 {
-    [self removeFromSuperview];
+    //临时方法，以后会修改
+    
+    pushButton *menuButton1 = self.btnArray[1];
+    if([anim isEqual:[menuButton1.layer animationForKey:@"animation1"]])
+    {
+        
+        menuButton1.center=[self.fromRectArray[1] CGRectValue].origin;
+    }
+    pushButton *menuButton0 = self.btnArray[0];
+    if([anim isEqual:[menuButton0.layer animationForKey:@"animation0"]])
+    {
+        
+        menuButton0.center=[self.fromRectArray[0] CGRectValue].origin;
+    }
+    
+    
+    pushButton *menuButton2 = self.btnArray[2];
+    if([anim isEqual:[menuButton2.layer animationForKey:@"animation2"]])
+    {
+        menuButton2.center=[self.fromRectArray[2] CGRectValue].origin;
+    }
+    
+    if([anim isEqual:[menuButton0.layer animationForKey:@"animationaClose0"]])
+    {
+        
+        [self removeFromSuperview];
+    }
+    
+    
+    
+    
 }
+
+
 
 
 @end
